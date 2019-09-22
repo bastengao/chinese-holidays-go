a3107cd8b3ea3e52d6fed7a5d42775419de809a8
bastengao <bastengao@gmail.com>
2019-09-22 12:39:31 +0800

    Expose query method

--- holidays/holidays.go
+++ holidays/holidays.go
@@ -1,17 +1,49 @@
 package holidays
 
 import (
+	"fmt"
 	"time"
 )
 
+var b *book
+
+func init() {
+	events, err := loadData()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bk, err := newBookfromEvents(events)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		b = &bk
+	}
+}
+
 // IsHoliday checks given date is holiday or not.
-func IsHoliday(date time.Time) bool {
-	// TODO
-	return false
+func IsHoliday(date time.Time) (bool, error) {
+	err := checkInitBook()
+	if err != nil {
+		return false, err
+	}
+
+	return b.isHoliday(date), nil
 }
 
 // IsWorkingday checks given date is working day or not.
-func IsWorkingday(date time.Time) bool {
-	// TODO
-	return false
+func IsWorkingday(date time.Time) (bool, error) {
+	err := checkInitBook()
+	if err != nil {
+		return false, err
+	}
+
+	return b.isWorkingday(date), nil
+}
+
+func checkInitBook() error {
+	if b == nil {
+		return fmt.Errorf("book initialize failed")
+	}
+	return nil
 }
--- holidays/holidays_test.go
+++ holidays/holidays_test.go
@@ -1,1 +1,30 @@
 package holidays
+
+import (
+	"testing"
+	"time"
+)
+
+func TestIsHoliday(t *testing.T) {
+	d := time.Date(2019, 10, 1, 0, 0, 0, 0, location)
+	result, err := IsHoliday(d)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !result {
+		t.Fail()
+	}
+}
+
+func TestIsWorkingday(t *testing.T) {
+	d := time.Date(2019, 9, 30, 0, 0, 0, 0, location)
+	result, err := IsWorkingday(d)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !result {
+		t.Fail()
+	}
+}
